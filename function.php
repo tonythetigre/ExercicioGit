457928be430bbf022982de08e2387b392d2a6842
Alunos <[rafaelpaspatuto@hotmail.com]>
2018-10-15 20:23:55 -0300

    alterando pela segunda  vez

--- function.php
+++ function.php
@@ -2,4 +2,10 @@
 	function umaFuncao(){
 		echo "Uma funcao que imprime."PHP_EOL;
 	}
+	function outraFuncao($nome){
+		echo "Ola,{$nome}! Eu sou uma funcao! "PHP_EOL;	
+	}
+	function novaFuncao($msg){
+		echo"Sou uma funcao que recebe:{$msg}."PHP_EOL;
+	}
 ?>
